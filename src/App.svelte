b9faa5086d69c5cde1206bed6828ad04baeb48cc
Csaba Kiss <cocmangamal@gmail.com>
2020-04-11 12:58:53 +0200

    Project setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Save Sheet</h1>
+	<p>Welcome to Save Sheet! Start a new campaign, or login to load an existing one.</p>
 </main>
 
 <style>
