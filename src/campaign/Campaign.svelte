34c177b400e487734506c95fe4e9869ef0dd7ddf
Csaba Kiss <cocmangamail@gmail.com>
2020-04-12 13:30:34 +0200

    Added campaign component

--- src/campaign/Campaign.svelte
+++ src/campaign/Campaign.svelte
@@ -1,17 +1,18 @@
 <script>
   import { onMount } from 'svelte';
+  import List, {Item, Text} from '@smui/list';
   import axios from 'axios';
   import NProgress from 'nprogress';
 
   export let capmaignId = 0;
 
-  let campaignState = [];
+  let campaignStatuses = [];
   let isLoading = true;
 
   onMount(async () => {
     NProgress.start();
     const response = await axios.get(`/statuses/${capmaignId}.json`);
-    campaignState = response.data;
+    campaignStatuses = response.data;
     isLoading = false;
     NProgress.done();
   });
@@ -19,5 +20,15 @@
 </script>
 
 <div>
-  { JSON.stringify(campaignState) }
+  {#if isLoading}
+    Loading...
+  {:else}
+    <List nonInteractive>
+      {#each campaignStatuses as status}
+        <Item>
+          <Text>{status.name}</Text>
+        </Item>
+      {/each}
+    </List>
+  {/if}
 </div>
