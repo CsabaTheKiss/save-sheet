de141691ad8c3bce3c1471026fc756a691f3a77a
Csaba Kiss <cocmangamail@gmail.com>
2020-04-12 11:12:49 +0200

    Layout update

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,24 @@
 <script>
   import Button, { Label } from '@smui/button';
   import { Route, Router, navigate } from 'svelte-routing';
+
+  import Header from './common/header/Header.svelte';
   import NewCampaing from './new-campaing/NewCampaing.svelte';
 
   export let url = '';
 </script>
 
 <Router { url }>
-  <main>
-    <h1>Save Sheet</h1>
-    <Route path="/new-campaing">
-      <NewCampaing />
-    </Route>
-    <Route path="/">
-      <p>Welcome to Save Sheet! Start a new campaign, or login to load an existing one.</p>
-      <Button color="secondary" on:click={() => navigate('/new-campaing')} variant="unelevated">
-        <Label>Start New Campaing</Label>
-      </Button>
-    </Route>
-  </main>
+  <Header></Header>
+  <Route path="/new-campaing">
+    <NewCampaing />
+  </Route>
+  <Route path="/">
+    <p>Welcome to Save Sheet! Start a new campaign, or login to load an existing one.</p>
+    <Button color="secondary" on:click={() => navigate('/new-campaing')} variant="unelevated">
+      <Label>Start New Campaing</Label>
+    </Button>
+  </Route>
 </Router>
 
 <style>
---
+++ src/common/header/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+  import IconButton from '@smui/icon-button';
+  import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+  import { Route, Router, navigate } from 'svelte-routing';
+</script>
+
+<div class="top-app-bar-container">
+  <TopAppBar variant="static">
+    <Row>
+      <Section>
+        <IconButton class="material-icons">menu</IconButton>
+        <Title>Static</Title>
+      </Section>
+      <Section align="end" toolbar>
+        <IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+        <IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+        <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+      </Section>
+    </Row>
+  </TopAppBar>
+</div>
